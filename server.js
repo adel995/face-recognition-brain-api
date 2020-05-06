1e8cb88ad1876ee76aae7f6fbc72acfe0e1570bc
adel995 <adbatal95@gmail.com>
2020-05-06 22:33:16 +0300

    deleted db for test

--- server.js
+++ server.js
@@ -2,28 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex');
+// const knex = require('knex');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const image = require('./controllers/image');
 const profile = require('./controllers/profile');
 
-const db = knex({
-    client: 'pg',
-    connection: {
-        host: '127.0.0.1',
-        user: 'edd',
-        password: 'e',
-        database: 'smart-brain'
-    }
-});
+// const db = knex({
+//     client: 'pg',
+//     connection: {
+//         host: '127.0.0.1',
+//         user: 'edd',
+//         password: 'e',
+//         database: 'smart-brain'
+//     }
+// });
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {res.send(db.users)});
+app.get('/', (req, res) => {'it is working'});
 
 app.post('/signin', signin.signinHandler(db, bcrypt));
 app.post('/register', register.registerHandler(db, bcrypt));
