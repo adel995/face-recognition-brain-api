dd6d50baebcd17f98ff1f2ade3e96cd87b6265cc
adel995 <adbatal95@gmail.com>
2020-05-06 22:37:30 +0300

    deleted db send for test

--- server.js
+++ server.js
@@ -2,22 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-// const knex = require('knex');
+const knex = require('knex');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const image = require('./controllers/image');
 const profile = require('./controllers/profile');
 
-// const db = knex({
-//     client: 'pg',
-//     connection: {
-//         host: '127.0.0.1',
-//         user: 'edd',
-//         password: 'e',
-//         database: 'smart-brain'
-//     }
-// });
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'edd',
+        password: 'e',
+        database: 'smart-brain'
+    }
+});
 
 const app = express();
 app.use(bodyParser.json());
