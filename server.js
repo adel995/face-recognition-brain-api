0acc02b2fb82d14672ddfc768fbbb14cd6d44463
adel995 <adbatal95@gmail.com>
2020-05-06 22:34:51 +0300

    deleted db for test

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {'it is working'});
+app.get('/', (req, res) => {res.send('it is working')});
 
 app.post('/signin', signin.signinHandler(db, bcrypt));
 app.post('/register', register.registerHandler(db, bcrypt));
