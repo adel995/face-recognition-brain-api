c852eedcd479ff7a136f82a8a0492a840de7d7db
adel995 <adbatal95@gmail.com>
2020-05-07 18:33:04 +0300

    a

--- controllers/image.js
+++ controllers/image.js
@@ -1,32 +1,30 @@
 const Clarifai = require('clarifai');
 
-//You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: '41425fc88809426aae0249f68280b2df'
-});
+    apiKey: '41425fc88809426aae0249f68280b2df'
+  });
 
-const handleApiCall = (req, res) => {
-  app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => res.status(400).json('unable to work with API'))
-}
+  const handleApiCall = () => (req, res) => {
+      app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+      .then(data => {
+          res.json(data)
+      })
+      .catch(err => res.status(400).json('unable to work with api'))
+  }
 
-const handleImage = (req, res, db) => {
-  const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
+const handleImage = (db) => (req, res) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
 }
 
 module.exports = {
-  handleImage,
-  handleApiCall
-}
-
+    handleImage,
+    handleApiCall
+};
