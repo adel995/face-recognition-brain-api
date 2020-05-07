87efff8be9502ac2228f87c185b0a23c6f4318b9
adel995 <adbatal95@gmail.com>
2020-05-07 14:17:55 +0300

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const profile = require('./controllers/profile');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-closed-09444',
         user: 'edd',
         password: 'e',
         database: 'smart-brain'
